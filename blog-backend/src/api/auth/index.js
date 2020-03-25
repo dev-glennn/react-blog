4478673fdeaab74261f43fca0f01f2f0b4cb175f
dev-glennn <dev.glennn@gmail.com>
2020-03-25 11:43:26 +0900

    feat: 회원가입, 로그인 기능 추가

--- blog-backend/src/api/auth/index.js
+++ blog-backend/src/api/auth/index.js
@@ -4,8 +4,8 @@ import * as authCtrl from './auth.ctrl';
 const auth = new Router();
 
 auth.post('/register', authCtrl.register);
-auth.post('/login', authCtrl.register);
-auth.get('/check', authCtrl.register);
-auth.post('/logout', authCtrl.register);
+auth.post('/login', authCtrl.login);
+auth.get('/check', authCtrl.check);
+auth.post('/logout', authCtrl.logout);
 
 export default auth;

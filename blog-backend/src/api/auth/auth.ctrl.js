45d187cb423fd54e3b1ea690c1c0fde9bff553b4
dev-glennn <dev.glennn@gmail.com>
2020-03-25 12:39:41 +0900

    feat: 토큰 발급하기

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -37,6 +37,12 @@ export const register = async ctx => {
 
         // 응답할 데이터에서 hashedPassword 필드 제거
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7, //7일
+            httpOnly: true,
+        });
     } catch (e) {
         ctx.throw(500, e);
     }
@@ -72,6 +78,11 @@ export const login = async ctx => {
             return;
         }
         ctx.body = user.serialize();
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7, //7일
+            httpOnly: true,
+        });
     } catch (e) {
         ctx.throw(500, e);
     }

23c83a99fb99007367f959a56341b87849892e32
dev-glennn <dev.glennn@gmail.com>
2020-03-25 15:58:00 +0900

    feat: 포스트 수정 및 삭제 시 권한 확인

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -4,10 +4,33 @@ import Joi from 'joi';
 
 const {ObjectId} = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
     const {id} = ctx.params;
     if (!ObjectId.isValid(id)) {
         ctx.status = 400;
+        return;
+    }
+    try {
+        const post = await Post.findById(id);
+        // 포스트가 존재하지 않을 때
+        if (!post) {
+            ctx.status = 404; // Not Found
+            return;
+        }
+        ctx.state.post = post;
+        return next();
+    } catch (e) {
+        ctx.throw(e);
+    }
+}
+
+/*
+로그인 중인 사용자가 작성한 포스트인지 확인해주는 func
+ */
+export const checkOwnPost = (ctx, next) => {
+    const {user, post} = ctx.state;
+    if (post.user._id.toString() !== user._id) {
+        ctx.status = 403;
         return;
     }
     return next();
@@ -92,17 +115,7 @@ export const list = async ctx => {
  * @returns {Promise<void>}
  */
 export const read = async ctx => {
-    const {id} = ctx.params;
-    try {
-        const post = await Post.findById(id).exec();
-        if (!post) {
-            ctx.status = 404; // Not Found
-            return;
-        }
-        ctx.body = post;
-    } catch (e) {
-        ctx.throw(500, e)
-    }
+    ctx.body = ctx.state.post;
 };
 
 /**

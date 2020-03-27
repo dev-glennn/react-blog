53134965ee1af1e247a46ee2f5c6a3ff3ef9c4bf
dev-glennn <dev.glennn@gmail.com>
2020-03-27 15:10:45 +0900

    hot: PostPage 수정

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import HeaderContainer from "../containers/common/HeaderContainer";
 import PostViewerContainer from "../containers/post/PostViewerContainer";
-import PostList from "../components/posts/PostList";
 
 const PostPage = () => {
     return (
         <>
             <HeaderContainer/>
-            <PostList/>
+            <PostViewerContainer/>
         </>
     );
 };
